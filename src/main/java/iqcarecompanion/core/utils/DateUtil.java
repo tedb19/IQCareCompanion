0024ef25a4559490818a6b67cf78f3053dc38f24
tedb19 <tedb19@gmail.com>
2015-11-28 23:53:17 +0300

    Added a test for DateUtil class Refactored the HAPIWrappers test Deleted the unused method in DateUtil

--- src/main/java/iqcarecompanion/core/utils/DateUtil.java
+++ src/main/java/iqcarecompanion/core/utils/DateUtil.java
@@ -1,24 +1,14 @@
 
 package iqcarecompanion.core.utils;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 /**
  *
  * @author Teddy Odhiambo
  */
 public class DateUtil {
-    public static String parseDate(String dateString) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd hh:mm:ss",
-                Locale.ENGLISH);
-        
-        Date parsedDate = sdf.parse(dateString);
-        SimpleDateFormat print = new SimpleDateFormat("yyyyMMddhhmmss");
-        return print.format(parsedDate);
-    }
 
     public static String parseDate(Date date) {
         SimpleDateFormat ft = new SimpleDateFormat("yyyyMMddhhmmss");
