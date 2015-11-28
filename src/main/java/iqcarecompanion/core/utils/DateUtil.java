867a6c4df7b1105a1b02076e79f22bb11ccaa888
tedb19 <tedb19@gmail.com>
2015-11-28 23:11:58 +0300

    Renamed the DateUtil class

--- src/main/java/iqcarecompanion/core/utils/DateUtil.java
+++ src/main/java/iqcarecompanion/core/utils/DateUtil.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package iqcarecompanion.core.utils;
 
@@ -13,9 +8,9 @@ import java.util.Locale;
 
 /**
  *
- * @author Teddy
+ * @author Teddy Odhiambo
  */
-public class ParseDateAndName {
+public class DateUtil {
     public static String parseDate(String dateString) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd hh:mm:ss",
                 Locale.ENGLISH);
