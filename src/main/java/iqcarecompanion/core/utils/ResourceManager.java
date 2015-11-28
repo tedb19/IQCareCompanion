3c3895e4a21e56e908f38c52e893c20860b48290
tedb19 <tedb19@gmail.com>
2015-11-29 01:18:20 +0300

    Wrote tests for the JSON file
    -validation of the fields
    -validation of the datatypes

--- src/main/java/iqcarecompanion/core/utils/ResourceManager.java
+++ src/main/java/iqcarecompanion/core/utils/ResourceManager.java
@@ -7,14 +7,10 @@ package iqcarecompanion.core.utils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import iqcarecompanion.core.jsonMapper.Event;
@@ -28,20 +24,16 @@ public class ResourceManager extends PropertiesManager {
 
     final static Logger logger = Logger.getLogger(ResourceManager.class.getName());
 
-    public static List<Event> readJSONFile(String jsonFile) {
-        List<Event> events = new ArrayList<>();
-        try {
-            byte[] jsonData = Files.readAllBytes(Paths.get(confPath + jsonFile));
-            ObjectMapper mapper = new ObjectMapper();
-            events = mapper.readValue(jsonData, new TypeReference<List<Event>>() {
-            });
-        } catch (IOException ex) {
-            logger.log(Level.SEVERE, ex.toString(), ex);
-        }
+    public static List<Event> readJSONFile(String jsonFile) throws IOException {
+        byte[] jsonData = Files.readAllBytes(Paths.get(confPath + jsonFile));
+        ObjectMapper mapper = new ObjectMapper();
+        List<Event> events = mapper.readValue(jsonData, new TypeReference<List<Event>>() {
+        });
+
         return events;
     }
-    
-    public static void updateLastId(int finalId, String key){
+
+    public static void updateLastId(int finalId, String key) {
         if (finalId != 0) {
             try {
                 ResourceManager.modifyConfigFile(key, Integer.toString(finalId), "runtime.properties");
